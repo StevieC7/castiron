4c361aed68cfc63c8ecd64b624aeea4ca8894b01
StevieC7 <stevenrcalhoun@outlook.com>
2023-05-27 12:06:52 -0500

    Add control for add / list mode

--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,46 @@ use std::io::prelude::*;
 use reqwest::{self, get};
 use tokio;
 fn main() {
-    // TODO: put in a function for asking user which mode they want (add new, fetch existing)
-    // TODO: put a function in to save URLs from user input into a file
-    // first, check if there is a file for user already
-    let open_file: Option<File> = get_feed_list();
-    match open_file {
-        Some(mut file) => {
-            println!("What feed do you want to update?");
-            let mut input_url: String = String::new();
-            io::stdin()
-            .read_line(&mut input_url)
-            .expect("Failed to read input.");
-            // if file, append url to list of saved URLs
-            let feed_xml = get_request(input_url);
-            match feed_xml {
-                Ok(xml) => {
-                    println!("{}", xml);
-                    let write_result = file.write(xml.as_bytes());
-                    match write_result {
-                        Ok(ok) => println!("Wrote to file: {}", ok),
-                        Err(e) => println!("Error writing to file: {}", e)
-                    }
-                },
-                Err(e) => println!("Error getting feed: {}", e)
-            }   
-        }
-        None => ()
-    }
-    // TODO: put a function in to read URLS from a file
     // TODO: make sure the function for fetching and saving from saved URLs runs on launch
-    // TODO: put a function in to save text retrieved from URL
+    println!("ADD podcast or LIST shows?");
+    let mut mode_selection: String = String::new();
+    io::stdin()
+    .read_line(&mut mode_selection)
+    .expect("Failed to read input.");
+    match mode_selection.as_str().trim() {
+        "ADD" => {
+            println!("You picked ADD");
+            // first, check if there is a file for user already
+            let open_file: Option<File> = get_feed_list();
+            match open_file {
+                Some(mut file) => {
+                    println!("What feed do you want to update?");
+                    let mut input_url: String = String::new();
+                    io::stdin()
+                    .read_line(&mut input_url)
+                    .expect("Failed to read input.");
+                    // if file, append url to list of saved URLs
+                    let feed_xml = get_request(input_url);
+                    match feed_xml {
+                        Ok(xml) => {
+                            println!("{}", xml);
+                            let write_result = file.write(xml.as_bytes());
+                            match write_result {
+                                Ok(ok) => println!("Wrote to file: {}", ok),
+                                Err(e) => println!("Error writing to file: {}", e)
+                            }
+                        },
+                        Err(e) => println!("Error getting feed: {}", e)
+                    }   
+                }
+                None => ()
+            }
+            // TODO: put a function in to save URLs from user input into a file
+        },
+        "LIST" => println!("You picked LIST"),
+        // TODO: put a function in to read URLS from a file
+        _ => println!("You picked wrong.")
+    }
 }
 
 #[tokio::main]
@@ -42,29 +52,16 @@ async fn get_request(url: String) -> Result<String, reqwest::Error> {
     Ok(result)
 }
 
-fn create_feed_list(path: &Path) -> std::io::Result<File> {
-    let mut file = File::create(path)?;
-    file.write_all(b"Hello, world!")?;
-    Ok(file)
-}
-
 fn get_feed_list() -> Option<File> {
     let path = Path::new( "./foo.txt");
-    let existing_file = fs::File::create(path);
-    match existing_file {
+    let file = fs::File::create(path);
+    match file {
         Ok( file ) => {
-            println!("file exists {:?}", file);
             Some(file)
         },
-        _ => {
-            // if no file, create one
-            match create_feed_list(path) {
-                Ok(file) => Some(file),
-                Err(e) => {
-                    println!("Error happened {}", e);
-                    None
-                }
-            }
+        Err(e) => { 
+            println!("Error creating file: {}", e);
+            None
         }
     }
 }
