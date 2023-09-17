7ab7ee42e0d47457635ccd23a04a10d2894f914e
StevieC7 <stevenrcalhoun@outlook.com>
2023-09-17 17:01:14 -0500

    Add check for existing path and plan for saving path names

--- src/main.rs
+++ src/main.rs
@@ -82,37 +82,48 @@ fn update_feeds(feeds: Vec<FeedMeta>) {
         match updated_feed {
             Ok(_val) => {
                 println!("Fetched feed: {:?}", feed.feed_url);
-                // TODO: see what directories exist, then make unique directory for this show
                 let mut path_string: String = format!("./shows/{:?}", rand::thread_rng().gen_range(0..10000));
-                println!("{path_string}");
-                let dir_path: &Path = Path::new(path_string.as_str());
-                let created_dir: Result<(), io::Error> = create_dir_all(dir_path);
-                match created_dir {
-                    Ok(_) => {
-                        path_string.push_str("/feed.xml");
-                        let file_path = Path::new(path_string.as_str());
-                        let xml_file = OpenOptions::new()
-                            .create(true)
-                            .write(true)
-                            .open(file_path);
-                        match xml_file {
-                            Ok(mut file) => {
-                                let seek_result: Result<u64, io::Error> = file.seek(io::SeekFrom::Start(0));
-                                match seek_result {
-                                    Ok(_) => {
-                                        let result = file.write_all(_val.as_bytes());
-                                        match result {
-                                            Ok(_) => println!("Successfully created xml file for show: {:?}", feed.feed_url),
-                                            Err(e) => println!("Error writing fetched data to xml file: {e}")
+                let path_exists = check_feed_exists(path_string.clone());
+                match path_exists {
+                    Ok(exists) => {
+                        if exists {
+                            println!("Path already exists, so we better generate a new one.")
+                            // TODO: write the directory name to the list of feeds to reference later
+                        } else {
+                            println!("Path does not exist, so carry on as usual.");
+                            let dir_path: &Path = Path::new(path_string.as_str());
+                            let created_dir: Result<(), io::Error> = create_dir_all(dir_path);
+                            match created_dir {
+                                Ok(_) => {
+                                    path_string.push_str("/feed.xml");
+                                    let file_path = Path::new(path_string.as_str());
+                                    let xml_file = OpenOptions::new()
+                                    .create(true)
+                                    .write(true)
+                                    .open(file_path);
+                                match xml_file {
+                                    Ok(mut file) => {
+                                        let seek_result: Result<u64, io::Error> = file.seek(io::SeekFrom::Start(0));
+                                        match seek_result {
+                                            Ok(_) => {
+                                                let result = file.write_all(_val.as_bytes());
+                                                // TODO: write the directory name to the list of feeds to reference later
+                                                match result {
+                                                    Ok(_) => println!("Successfully created xml file for show: {:?}", feed.feed_url),
+                                                    Err(e) => println!("Error writing fetched data to xml file: {e}")
+                                                }
+                                            },
+                                            Err(e) => println!("Error seeking for write head: {e}")
                                         }
                                     },
-                                    Err(e) => println!("Error seeking for write head: {e}")
+                                    Err(e) => println!("Error creating xml file: {e}")
                                 }
                             },
-                            Err(e) => println!("Error creating xml file: {e}")
+                            Err(e) => println!("Error creating directory: {e}")
+                        }
                         }
                     },
-                    Err(e) => println!("Error creating directory: {e}")
+                    Err(_) => ()
                 }
             },
             Err(e) => println!("Error fetching feed: {e}")
@@ -138,12 +149,13 @@ fn get_feed_list() -> Option<File> {
 }
 #[derive(Serialize, Deserialize, Debug)]
 struct FeedMeta {
-    feed_url: String
+    feed_url: String,
+    xml_file_path: Option<String>
 }
 
 fn add_feed_to_list(url: String, mut file: File) -> Option<File> {
     let trimmed_url = url.trim().to_string();
-    let feed_meta: FeedMeta = FeedMeta { feed_url: trimmed_url };
+    let feed_meta: FeedMeta = FeedMeta { feed_url: trimmed_url, xml_file_path: None };
     let json_feed = serde_json::to_string(&feed_meta);
     match json_feed {
         Ok( _feed ) => {
@@ -217,5 +229,30 @@ fn add_feed_to_list(url: String, mut file: File) -> Option<File> {
             println!("An error occurred: {e}");
             None
         }
+    }
+}
+
+fn check_feed_exists (comparison_path: String) -> Result<bool, serde_json::Error> {
+    let read_file: String = fs::read_to_string(Path::new("./feed_list.json")).expect("Oopsie reading saved file.");
+    let contents: Result<Vec<FeedMeta>, serde_json::Error> = serde_json::from_str( & read_file );
+    match contents {
+        Ok(values) => {
+            if values.len() == 0 {
+                return Ok(false);
+            } else {
+                for content in values {
+                    match content.xml_file_path {
+                        Some(val) => {
+                            if val == comparison_path {
+                                return Ok(true);
+                            }
+                        },
+                        None => ()
+                    }
+                }
+                return Ok(false);
+            }
+        },
+        Err(e) => Err(e)
     }
 }
