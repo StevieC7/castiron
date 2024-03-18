40b048a66a2a7194ef60811da509849ebac73dd2
Steven Calhoun <stevenrcalhoun@outlook.com>
2024-03-18 14:33:56 -0500

    Add nasty bit of code to parse existing xml
    Control flow pain

--- src/networking/downloads.rs
+++ src/networking/downloads.rs
@@ -1,5 +1,6 @@
 use reqwest::{get, Error as ReqwestError};
 use roxmltree::Document;
+use std::fs;
 use tokio;
 
 //
@@ -14,23 +15,62 @@ use tokio;
 // download the single latest episode
 //
 pub fn download_episodes() -> Option<String> {
-    let doc = Document::parse("<title>You Suck</title>");
-    match doc {
-        Ok(stuff) => {
-            let ready_val = stuff.descendants().find(|n| n.has_tag_name("title"))?;
-            match ready_val.text() {
-                Some(thing) => {
-                    println!("{thing}");
-                    Some(thing.to_string())
+    // iterate over existing feeds in /shows directory
+    let feed_path_list =
+        fs::read_dir("./shows").expect("Show directory is missing or improperly formatted.");
+
+    let mut feed_collection = Vec::new();
+    for feed in feed_path_list {
+        match feed {
+            Ok(directory) => {
+                let feed_xml_file = fs::read_dir(directory.path());
+                match feed_xml_file {
+                    Ok(mut xml_file) => {
+                        let file_path = xml_file.next();
+                        match file_path {
+                            Some(thing) => match thing {
+                                Ok(final_path) => {
+                                    let actual_path = final_path.path();
+                                    let feed_content = fs::read_to_string(actual_path);
+                                    match feed_content {
+                                        Ok(content) => {
+                                            feed_collection.push(content);
+                                        }
+                                        Err(e) => println!("{e}"),
+                                    }
+                                }
+                                Err(e) => println!("{e}"),
+                            },
+                            None => (),
+                        }
+                    }
+                    Err(e) => println!("{e}"),
                 }
-                None => None,
             }
-        }
-        Err(e) => {
-            println!("{e}");
-            None
+            Err(e) => println!("{e}"),
         }
     }
+    println!("{:?}", feed_collection.len());
+    for feed in feed_collection {
+        let fmt_string = feed.as_str();
+        let doc = Document::parse(fmt_string);
+        match doc {
+            Ok(stuff) => {
+                let ready_val = stuff.descendants().find(|n| n.has_tag_name("title"))?;
+                match ready_val.text() {
+                    Some(thing) => {
+                        println!("{thing}");
+                    }
+                    None => (),
+                }
+            }
+            Err(e) => {
+                println!("{e}");
+                ()
+            }
+        }
+    }
+    None
 }
 
 #[tokio::main]
