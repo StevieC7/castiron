3f8c8af44db2213487a3d2f428df9bea319d1616
Steven Calhoun <stevenrcalhoun@outlook.com>
2024-05-09 17:42:54 -0500

    shit

---
+++ src/types/episodes.rs
@@ -0,0 +1,9 @@
+pub struct Episode {
+    pub index: i32,
+    pub guid: String,
+    pub title: String,
+    pub date: String,
+    pub played: bool,
+    pub played_seconds: i32,
+    pub feed_id: i32,
+}
---
+++ src/ui/gui.rs
@@ -0,0 +1,53 @@
+use iced::widget::{button, column, text};
+use iced::{Alignment, Element, Sandbox};
+
+use super::widgets::Episode;
+
+pub struct Counter {
+    value: i32,
+    episodes: Vec<Episode>,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    IncrementPressed,
+    DecrementPressed,
+    OpenPlayer,
+}
+
+impl Sandbox for Counter {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self { value: 0 }
+    }
+
+    fn title(&self) -> String {
+        String::from("Counter - Iced")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::IncrementPressed => {
+                self.value += 1;
+            }
+            Message::DecrementPressed => {
+                self.value -= 1;
+            }
+            Message::OpenPlayer => {
+                println!("It's happening")
+            }
+        }
+    }
+
+    fn view(&self) -> Element<Message> {
+        column![
+            button("Increment").on_press(Message::IncrementPressed),
+            text(self.value).size(50),
+            button("Decrement").on_press(Message::DecrementPressed),
+        ]
+        .padding(20)
+        .align_items(Alignment::Center)
+        .into()
+    }
+}
