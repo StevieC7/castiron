6aa855d3f9420a317f5355dbca3e8d7462bd5702
Steven Calhoun <stevenrcalhoun@outlook.com>
2024-05-13 12:56:26 -0500

    Add create_config

--- src/file_handling/config.rs
+++ src/file_handling/config.rs
@@ -1,6 +1,12 @@
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+    path::Path,
+};
+
 use crate::types::config::CastironConfig;
 use crate::types::errors::CustomError;
-use serde_json::{Deserializer, Serializer};
+use serde_json::to_writer;
 
 pub fn read_config() -> Result<CastironConfig, CustomError> {
     Ok(CastironConfig {
@@ -11,5 +17,10 @@ pub fn read_config() -> Result<CastironConfig, CustomError> {
 }
 
 pub fn create_config(config: CastironConfig) -> Result<(), CustomError> {
+    let config_file_path = Path::new("./castiron_config.json");
+    let config_file = File::create(config_file_path)?;
+    let mut writer = BufWriter::new(config_file);
+    to_writer(&mut writer, &config)?;
+    writer.flush()?;
     Ok(())
 }
--- src/types/config.rs
+++ src/types/config.rs
@@ -1,3 +1,6 @@
+use serde::Serialize;
+
+#[derive(Serialize)]
 pub struct CastironConfig {
     pub auto_dl_new: bool,
     pub auto_rm_after_listen: bool,
