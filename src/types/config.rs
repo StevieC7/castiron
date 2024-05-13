e8f55a8b1cb5311c979b274941ddbe8028fdb4b8
Steven Calhoun <stevenrcalhoun@outlook.com>
2024-05-13 14:12:17 -0500

    Add read_config

--- src/types/config.rs
+++ src/types/config.rs
@@ -1,6 +1,6 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 pub struct CastironConfig {
     pub auto_dl_new: bool,
     pub auto_rm_after_listen: bool,
