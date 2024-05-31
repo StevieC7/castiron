540801f60bdcfdb95095a57fb7fd9933421cbf9c
Steven Calhoun <stevenrcalhoun@outlook.com>
2024-05-31 17:57:46 -0500

    Move TUI mode functionality into TUI mod

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ mod networking;
 mod types;
 mod ui;
 
-use crate::networking::{downloads::download_episodes, feeds::update_feeds};
 use crate::ui::tui::tui_loop;
 use iced::{Application, Settings};
 use std::env::args;
@@ -17,31 +16,9 @@ async fn main() -> iced::Result {
             if flag == String::from("--gui") {
                 AppLayout::run(Settings::default())
             } else {
-                let feed_list_result = update_feeds().await;
-                match feed_list_result {
-                    Ok(_) => println!("Finished updating feeds."),
-                    Err(e) => println!("Error occurred while fetching feed list: {:?}", e),
-                }
-                let download_result = download_episodes().await;
-                match download_result {
-                    Ok(_) => println!("Finished downloading episodes."),
-                    Err(e) => println!("Error occurred while downloading episodes: {:?}", e),
-                }
                 Ok(tui_loop().await)
             }
         }
-        None => {
-            let feed_list_result = update_feeds().await;
-            match feed_list_result {
-                Ok(_) => println!("Finished updating feeds."),
-                Err(e) => println!("Error occurred while fetching feed list: {:?}", e),
-            }
-            let download_result = download_episodes().await;
-            match download_result {
-                Ok(_) => println!("Finished downloading episodes."),
-                Err(e) => println!("Error occurred while downloading episodes: {:?}", e),
-            }
-            Ok(tui_loop().await)
-        }
+        None => Ok(tui_loop().await),
     }
 }
--- src/ui/tui.rs
+++ src/ui/tui.rs
@@ -1,10 +1,23 @@
 use async_recursion::async_recursion;
 use std::io;
 
-use crate::file_handling::feeds::{add_feed_to_database, get_feed_list_database};
+use crate::{
+    file_handling::feeds::{add_feed_to_database, get_feed_list_database},
+    networking::{downloads::download_episodes, feeds::update_feeds},
+};
 
 #[async_recursion]
 pub async fn tui_loop() {
+    let feed_list_result = update_feeds().await;
+    match feed_list_result {
+        Ok(_) => println!("Finished updating feeds."),
+        Err(e) => println!("Error occurred while fetching feed list: {:?}", e),
+    }
+    let download_result = download_episodes().await;
+    match download_result {
+        Ok(_) => println!("Finished downloading episodes."),
+        Err(e) => println!("Error occurred while downloading episodes: {:?}", e),
+    }
     println!("ADD podcast, LIST shows");
     let mut mode_selection: String = String::new();
     io::stdin()
