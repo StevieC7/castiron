90879c7d2b0ef430180ff4ebe00d7102265e991d
Steven Calhoun <stevenrcalhoun@outlook.com>
2025-07-27 23:00:44 -0500

    Hashmaps?

--- src/ui/widgets/feed.rs
+++ src/ui/widgets/feed.rs
@@ -1,7 +1,7 @@
 use iced::{
     advanced::image::Handle,
     widget::{button, container, image, row, text},
-    Element, Length, Task,
+    Element, Length,
 };
 
 #[derive(Debug, Clone)]
@@ -15,70 +15,48 @@ pub struct Feed {
 pub enum Message {
     UnfollowFeed(i32),
     ViewEpisodesForShow(i32),
-    ImageLoaded(i32, Option<Handle>),
 }
 
 impl Feed {
-    pub fn new(
-        id: i32,
-        feed_title: String,
-        image_file_path: Option<String>,
-    ) -> (Self, Task<Message>) {
-        (
-            Self {
-                id,
-                feed_title,
-                image_handle: None,
-            },
-            Task::perform(
-                async {
-                    match image_file_path {
-                        Some(fp) => Some(Handle::from(fp)),
-                        None => None,
-                    }
-                },
-                move |handle| Message::ImageLoaded(id, handle),
-            ),
-        )
-    }
-    pub fn update(&mut self, message: Message) -> Task<Message> {
-        match message {
-            Message::ImageLoaded(id, handle) => {
-                if id == self.id {
-                    match handle {
-                        Some(image_handle) => {
-                            self.image_handle = Some(image_handle);
-                            Task::none()
-                        }
-                        None => Task::none(),
-                    }
-                } else {
-                    Task::none()
-                }
-            }
-            Message::UnfollowFeed(_id) => Task::none(),
-            Message::ViewEpisodesForShow(_id) => Task::none(),
+    pub fn new(id: i32, feed_title: String, image_handle: Option<Handle>) -> Self {
+        Self {
+            id,
+            feed_title,
+            image_handle,
         }
     }
     pub fn view(&self) -> Element<Message> {
-        let image = match &self.image_handle {
-            Some(handle) => image(handle),
-            None => image(""),
-        };
-        container(row!(
-            image.height(50),
-            text(self.feed_title.to_owned()).width(Length::FillPortion(6)),
-            button(text("Unfollow"))
-                .on_press(Message::UnfollowFeed(self.id))
-                .width(Length::FillPortion(3)),
-            button(text("View Episodes"))
-                .on_press(Message::ViewEpisodesForShow(self.id))
-                .width(Length::FillPortion(3))
-        ))
-        .max_width(600)
-        .padding(20)
-        .center_x(Length::Shrink)
-        // .center_y(Length::Fill)
-        .into()
+        if let Some(handle) = &self.image_handle {
+            container(row!(
+                image(handle).height(50),
+                text(self.feed_title.to_owned()).width(Length::FillPortion(6)),
+                button(text("Unfollow"))
+                    .on_press(Message::UnfollowFeed(self.id))
+                    .width(Length::FillPortion(3)),
+                button(text("View Episodes"))
+                    .on_press(Message::ViewEpisodesForShow(self.id))
+                    .width(Length::FillPortion(3))
+            ))
+            .max_width(600)
+            .padding(20)
+            .center_x(Length::Shrink)
+            // .center_y(Length::Fill)
+            .into()
+        } else {
+            container(row!(
+                text(self.feed_title.to_owned()).width(Length::FillPortion(6)),
+                button(text("Unfollow"))
+                    .on_press(Message::UnfollowFeed(self.id))
+                    .width(Length::FillPortion(3)),
+                button(text("View Episodes"))
+                    .on_press(Message::ViewEpisodesForShow(self.id))
+                    .width(Length::FillPortion(3))
+            ))
+            .max_width(600)
+            .padding(20)
+            .center_x(Length::Shrink)
+            // .center_y(Length::Fill)
+            .into()
+        }
     }
 }
--- src/ui/widgets/feed_list.rs
+++ src/ui/widgets/feed_list.rs
@@ -1,94 +1,114 @@
+use std::collections::HashMap;
+
 use super::feed::{Feed, Message as FeedMessage};
-use crate::file_handling::feeds::{delete_associated_episodes_and_xml, load_feeds};
+use crate::file_handling::feeds::{delete_associated_episodes_and_xml, get_feed_by_id, load_feeds};
 use crate::types::feeds::FeedMeta;
-use iced::widget::{column, container, text};
-use iced::{alignment::Horizontal, widget::Scrollable, Element, Length, Task};
+use iced::{
+    advanced::image::Handle,
+    alignment::Horizontal,
+    widget::{column, container, text, Scrollable},
+    Element, Length, Task,
+};
 
 pub struct FeedList {
-    feeds: Option<Vec<Feed>>,
+    feeds: HashMap<i32, Feed>,
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
     FeedMessage(FeedMessage),
     FeedsLoaded(Result<Vec<FeedMeta>, String>),
+    ImageLoaded(i32, Handle),
 }
 
 impl FeedList {
     pub fn new() -> Self {
-        Self { feeds: None }
+        Self {
+            feeds: HashMap::new(),
+        }
     }
     pub fn update(&mut self, message: Message) -> Task<Message> {
         match message {
-            Message::FeedMessage(feed_message) => {
-                if let Some(feeds) = &mut self.feeds {
-                    let mut tasks: Vec<Task<Message>> = feeds
-                        .iter_mut()
-                        .map(|feed| feed.update(feed_message.clone()).map(Message::FeedMessage))
-                        .collect();
-                    if let FeedMessage::UnfollowFeed(id) = feed_message {
-                        let load_feed_task = match delete_associated_episodes_and_xml(id) {
-                            Ok(_) => Task::perform(load_feeds(), Message::FeedsLoaded),
-                            Err(e) => {
-                                eprintln!("Error deleting feed: {:?}", e);
-                                Task::none()
-                            }
-                        };
-                        tasks.push(load_feed_task);
+            Message::FeedMessage(feed_message) => match feed_message {
+                FeedMessage::UnfollowFeed(id) => match delete_associated_episodes_and_xml(id) {
+                    Ok(_) => Task::perform(load_feeds(), Message::FeedsLoaded),
+                    Err(e) => {
+                        eprintln!("Error deleting feed: {:?}", e);
+                        Task::none()
                     }
-                    Task::batch(tasks)
-                } else {
-                    Task::none()
-                }
-            }
+                },
+                FeedMessage::ViewEpisodesForShow(_id) => Task::none(),
+            },
             Message::FeedsLoaded(list) => match list {
                 Err(_) => Task::none(),
                 Ok(data) => match data.len() {
                     0 => Task::none(),
                     _ => {
-                        let mut tasks: Vec<Task<Message>> = Vec::new();
-                        let list = data
-                            .iter()
-                            .map(|n| {
-                                let Some(image_path) = &n.image_file_path else {
-                                    todo!()
-                                };
-                                let Some(feed_title) = &n.feed_title else {
-                                    todo!()
-                                };
-                                let (element, task) = Feed::new(
-                                    n.id,
-                                    feed_title.to_owned(),
-                                    Some(image_path.to_owned()),
-                                );
-                                tasks.push(task.map(Message::FeedMessage));
-                                element
-                            })
-                            .collect();
-                        self.feeds = Some(list);
-                        Task::batch(tasks)
+                        let mut hash: HashMap<i32, Feed> = HashMap::new();
+                        let mut task_batch: Vec<Task<Message>> = Vec::new();
+                        data.into_iter().for_each(|n| {
+                            let Some(feed_title) = &n.feed_title else {
+                                todo!()
+                            };
+                            let feed = Feed::new(n.id, feed_title.to_owned(), None);
+                            hash.insert(n.id, feed);
+                            task_batch.push(Task::perform(
+                                async move {
+                                    (
+                                        n.id,
+                                        Handle::from_path(
+                                            n.image_file_path
+                                                .to_owned()
+                                                .unwrap_or(String::from("")),
+                                        ),
+                                    )
+                                },
+                                |(id, handle)| Message::ImageLoaded(id, handle),
+                            ));
+                        });
+                        self.feeds = hash;
+                        // NOW: fix this
+                        Task::batch(task_batch)
                     }
                 },
             },
+            Message::ImageLoaded(id, handle) => {
+                // TODO: initiate rebuild of affected feeds
+                let new_feed = get_feed_by_id(id);
+                match new_feed {
+                    Ok(meta) => {
+                        let replace_with = Feed::new(
+                            meta.id,
+                            meta.feed_title.unwrap_or(String::from("")),
+                            Some(handle),
+                        );
+                        self.feeds.insert(id, replace_with);
+                        Task::none()
+                    }
+                    Err(_) => Task::none(),
+                }
+            }
         }
     }
     pub fn view(&self) -> Element<Message> {
-        match &self.feeds {
-            Some(list) => Scrollable::new(
+        if 0 == self.feeds.len() {
+            container(text("You don't follow any feeds yet."))
+                .padding(20)
+                .center_x(Length::Fill)
+                .into()
+        } else {
+            Scrollable::new(
                 column(
-                    list.iter()
-                        .map(|item| item.view().map(|m| Message::FeedMessage(m))),
+                    self.feeds
+                        .iter()
+                        .map(|(_id, item)| item.view().map(|m| Message::FeedMessage(m))),
                 )
                 .padding(20)
                 .align_x(Horizontal::Center),
             )
             .width(Length::Fill)
             .height(Length::Fill)
-            .into(),
-            None => container(text("You don't follow any feeds yet."))
-                .padding(20)
-                .center_x(Length::Fill)
-                .into(),
+            .into()
         }
     }
 }
